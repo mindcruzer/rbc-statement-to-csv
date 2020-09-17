cbc19be9deb7a3d7b5cab086ff63928649883ce7
Tommy Jerry Mairo <tommy.mairo@gmail.com>
2020-09-16 17:34:43 -0700

    Add: convert.sh
    Add bash script file for Linux and MacOS

---
+++ convert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# define output filename
+OUTPUT_FILE='transactions.csv'
+
+# find pdf2txt.py
+PDF2TXT=$(which pdf2txt.py)
+
+PDF_FILES=$(find -H . -type f -regextype posix-extended -regex '\./.+\.pdf' -print)
+
+for PDF in ${PDF_FILES};
+do
+	XML_FILE=$(basename -s .pdf "${PDF}")
+	python "${PDF2TXT}" -o "${XML_FILE}.xml" "${PDF}" 
+done
+
+XML_FILES=($(find -H . -type f -regextype posix-extended -regex '\./.+\.xml' -print))
+
+echo "${XML_FILES[@]}"
+python convert.py "${OUTPUT_FILE}" "${XML_FILES[@]}"
+
+rm -rf .xml
